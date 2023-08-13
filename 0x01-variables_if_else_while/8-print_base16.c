004ecc42bcff811c766a8f1f1163839b9c47bb8f
ahmedelmkawey <ahmedmkaway@gmail.com>
2023-08-12 21:02:10 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description :  the numbers of base 16 in lowercase
+ *
+ * Return: Always (0) (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+
+	}
+
+		putchar('\n');
+	return (0);
+}
