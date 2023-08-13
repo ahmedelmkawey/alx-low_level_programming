dd52e48b386f30f58218709b39df707cb83ca51e
ahmedelmkawey <ahmedmkaway@gmail.com>
2023-08-12 21:35:25 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,22 +2,22 @@
 /**
  * main - Entry point
  *
- * Description :  prints all possible combinations of single-digit numbers with ,
+ * Description : prints all possible combinations of single-digit numbers with
  *
  * Return: Always (0) (Success)
  */
 
 int main(void)
 {
-	int x=0;
-	
+	int x = 0;
+
 	while (x < 10)
 	{
 		putchar(x + '0');
-		if (x < 9)
+	if (x < 9)
 		{
-	       putchar(',');
-       		putchar(' ');	       
+		putchar(',');
+		 putchar(' ');	       
 		}
 		x++;
 	}
