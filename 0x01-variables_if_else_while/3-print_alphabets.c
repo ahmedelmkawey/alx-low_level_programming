6f081430898e9db0a33ccf93e5691b2f42bec1bc
ahmedelmkawey <ahmedmkaway@gmail.com>
2023-08-12 18:21:49 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,18 +3,8 @@
  * main - Entry point
  *
  * Description : that prints the alphabet in lowercase, followed by a new line.
- *
+ *and upper case
  * Return: Always (0) (Success)
  */
-
 int main(void)
 {
-	char ch;
-
-	for (ch = 'a'; ch < 'z'; ch++)
-	{
-		putchar(ch);
-
-	}
-	return (0);
-}
