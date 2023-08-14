9267de9dd0629e815766438227ce3e44d467cb6a
ahmedelmkawey <ahmedmkaway@gmail.com>
2023-08-13 21:04:42 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,13 +13,13 @@ int main(void)
 	int c;
 	int d;
 	
-	for ( a = 0 ; a < 10 ; a++ )
+	for (a = 0 ; a < 10 ; a++)
 	{
-		for ( b = 0 ; b <= 10 ; b++ )
+		for (b = 0 ; b <= 10 ; b++)
 		{
-			for ( c = 0 ; c < 10 ; c++ )
+			for (c = 0 ; c < 10 ; c++)
 			{
-				for ( d = 0 ; d <= 10 ; d++ )
+				for (d = 0 ; d <= 10 ; d++)
 				{
 					putchar('0' + a);
 					putchar('0' + b);
@@ -31,7 +31,7 @@ int main(void)
 						putchar(',');
 						putchar(32);
 					}
-					c++;
+					d++;
 				}
 			}
 		}
