b8e5d2cd332a1c70ca2c71357f614708715c4ac7
ahmedelmkawey <ahmedmkaway@gmail.com>
2023-08-17 16:29:38 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,10 @@ void more_numbers(void)
 	for (line = 0; line <= 9; line++)
 	{
 		for (num = '0'; num <= 14; num++)
+		{
 			_putchar(num);
-		_putchar('\n');
+		}	
+			_putchar('\n');
+		
 	}
 }
