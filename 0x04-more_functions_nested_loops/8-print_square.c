c294b4cbb6df894f560ee9d7ffeb68c0980ee7c5
ahmedelmkawey <ahmedmkaway@gmail.com>
2023-08-18 17:50:31 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+/**
+ *print_square  - check the code
+ *@size :  is the size of the square
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+				_putchar('\n');
+			}
+		}
+	}
+
+		else
+			_putchar('\n');
+}	
