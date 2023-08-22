ee24fe5b1c1bb760ad0069a5a5edb271dc69a955
ahmedelmkawey <ahmedmkaway@gmail.com>
2023-08-21 19:09:01 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - function to copy array
+ * @dest : pointer to an array
+ * @src : pointer to an array
+ * Return: Always 0.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a, b;
+
+	for (a = 0; src[a] != '\0'; a++)
+		;
+	for (b = 0; b < a; b++)
+		dest[b] = src[b];
+	return (dest);
+}
