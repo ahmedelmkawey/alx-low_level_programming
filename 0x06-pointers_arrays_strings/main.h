b54585321afffa416d7524be6ea97a911d256ec5
ahmedelmkawey <ahmedmkaway@gmail.com>
2023-08-24 18:14:22 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ *
+ * file main.h that comtail all prototype that used in tasks
+ *
+ */
+
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+
+
+
+
+#endif
