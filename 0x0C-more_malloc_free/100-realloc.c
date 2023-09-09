0136b4bdf5488468a186a370489ad31e2404e939
ahmedelmkawey <ahmedmkaway@gmail.com>
2023-09-08 18:34:08 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -5,29 +5,48 @@
 
 
 /**
- * _realloc - allocates memory for an array, using malloc.
- * @ptr: number of elements.
- * @old_size: element's size.
- * @new_size: element's size.
- * Return: returns a pointer to an array of elements.
+ * *_realloc - function with 3 parameter
+ * @ptr: pointer to old memory
+ * @old_size: size of old memory
+ * @new_size: size of new memory
+ *
+ * Description: allocate memory for new size
+ * Return: pointer to new memory allocated
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	char *np;
 	unsigned int i;
-	char *array;
 
 	if (new_size == old_size)
 		return (ptr);
+	if (ptr == NULL)
+	{
+		np = malloc(new_size);
+		if (np == NULL)
+		{
+			free(np);
+			return (NULL);
+		}
+		return (np);
+	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	array = malloc(new_size);
-	if (array == NULL)
+
+	np = malloc(new_size);
+	if (np == NULL)
+	{
+		free(np);
 		return (NULL);
-	for (i = 0; i < new_size; i++)
-		*(array + (i * new_size)) = 0;
-	return (array);
+	}
+
+	for (i = 0; i < old_size; i++)
+		np[i] = *((char *)ptr + i);
+
+	free(ptr);
+	return (np);
 }
