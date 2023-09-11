4860504bdddd5db0f1a43087809c2c1a0baff685
ahmedelmkawey <ahmedmkaway@gmail.com>
2023-09-11 15:37:28 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,4 @@
-include "dog.h"
+#include "dog.h"
 
 /**
  * init_dog - cinitialize a dog data structure.
@@ -11,7 +11,7 @@ include "dog.h"
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if(d)
+	if (d)
 	{
 	d->name = name;
 	d->age = age;
