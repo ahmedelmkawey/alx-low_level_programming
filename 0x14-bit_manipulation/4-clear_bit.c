c7b8c8ac8841c98937b6dc32bd168c6f0e4188f2
ahmedelmkawey <ahmedmkaway@gmail.com>
2023-09-28 19:39:25 -0700

    14

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * clear_bit - function with two arguments
+ * @n: pointer to value
+ * @index: position to change bit
+ * Return: 1 if worked and -1 if error
+ *
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > (sizeof(*n) * 8))
+		return (-1);
+
+	*n &= ~(1 << index);
+	return (1);
+
+}
